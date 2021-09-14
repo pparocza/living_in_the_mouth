80169ccf2cd36432e5accc622413a15948361e92
paparoc <paulparoczai@gmail.com>
2021-09-14 12:44:50 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+var offlineBuffer;
+var piece;
+
+setTimeout(function(){bufferLoaded();}, 1000);
+
+function bufferLoaded(){
+
+	piece = new Piece();
+	piece.initMasterChannel();
+
+	// INITIALIZATIONS
+
+	if(onlineButton.innerHTML == "online"){
+		setTimeout(function(){onlineBufferLoaded();}, 1000);
+	}
+
+	else if(onlineButton.innerHTML == "offline"){
+		offlineBufferLoaded();
+	}
+
+}
+
+//--------------------------------------------------------------
+
+function runPatch(){
+		
+		piece.initParams();
+		piece.start();
+
+}
+
+//--------------------------------------------------------------
+
+function stopPatch(){
+
+	piece.fadeFilter.start(0, 20);
+	setTimeout(function(){piece.masterGain.disconnect();}, 100);
+	startButton.innerHTML = "reset";
+
+	if(onlineButton.innerHTML=="offline"){
+		offlineBuffer.stop();
+	}
+
+}
+
+//--------------------------------------------------------------
+
+function onlineBufferLoaded(){
+
+	startButton.disabled = false;
+	startButton.innerHTML = "start";
+
+}
+
+//--------------------------------------------------------------
+
+function offlineBufferLoaded(){
+
+	runPatch();
+
+	audioCtx.startRendering().then(function(renderedBuffer){
+
+		offlineBuffer = onlineCtx.createBufferSource();
+		offlineBuffer.buffer = renderedBuffer
+
+		startButton.disabled = false;
+		startButton.innerHTML = "start";
+
+		offlineBuffer.connect(onlineCtx.destination);
+
+	})
+
+}
